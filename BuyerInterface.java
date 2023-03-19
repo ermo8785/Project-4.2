c5ee35f3d81ff72d96bd399d3bea204d1bcb3b01
Eric Morales <ermo8785@colorado.edu>
2023-03-18 19:02:09 -0600

    newchanges

--- BuyerInterface.java
+++ BuyerInterface.java
@@ -7,7 +7,7 @@ interface BuyerInterface {
     String execute(FNCD Choice);
 }
 
-class SalespersonName implements BuyerInterface, SysOut{ 
+class SalespersonName implements BuyerInterface, SysOut{  //Returns the name of the salesperson 
         public String execute(FNCD Choice){
             ArrayList <Staff> salespeople = Staff.getStaffByType(Choice.staff, Enums.StaffType.Salesperson); //Fills Array list with salespeople 
             int index_sales = Utility.rndFromRange(0,2); //Sets an index within the range 
@@ -16,53 +16,103 @@ class SalespersonName implements BuyerInterface, SysOut{
         }
 }
 
-class Time implements BuyerInterface, SysOut {
+class Time implements BuyerInterface, SysOut { //Returns the time 
     public String execute(FNCD Choice){
-        DateTimeFormatter Format = DateTimeFormatter.ofPattern("HH:MM:SS");
+        DateTimeFormatter Format = DateTimeFormatter.ofPattern("HH:MM:SS"); //Format for time used: https://www.javatpoint.com/java-get-current-date
         LocalDateTime CurrentTime = LocalDateTime.now();
         return(Format.format(CurrentTime));
     }
 }
 
 class CurrentInventory implements BuyerInterface, SysOut{
+    public ArrayList<Vehicle> vehicleInventory = new ArrayList<>(); //Array List for vehicles as Vehicles
+    public ArrayList<String> StringInventory = new ArrayList<>();//Array List for vehicles as Strings
+    StringBuilder CarInventory = new StringBuilder(); //builds new string 
+    public String execute(FNCD Choice){ 
+        for (Vehicle i: Choice.inventory){
+            CarInventory.append(i.name + "\n"); //adds to new string
+            vehicleInventory.add(i); //adds to array lost as vehicle 
+            StringInventory.add(i.name); //adds to array list as string 
+        }
+
+        return CarInventory.toString(); //Returns string 
+
+    }
+}
+
+class DisplaySelectedCar implements BuyerInterface, SysOut { //Uses function above code to fill arrays and display a selceted car.
     public ArrayList<Vehicle> vehicleInventory = new ArrayList<>();
     public ArrayList<String> StringInventory = new ArrayList<>();
     StringBuilder CarInventory = new StringBuilder();
-    public String execute(FNCD Choice){
+    StringBuilder CarDetails = new StringBuilder();
+    public String execute(FNCD Choice) {
         for (Vehicle i: Choice.inventory){
             CarInventory.append(i.name + "\n");
             vehicleInventory.add(i);
             StringInventory.add(i.name);
         }
-        /*
-        for(int i = 0; i < StringInventory.size(); i++){
-            out(StringInventory.get(i));
-        }
-        //out(CarInventory.toString());
-        */
-        return CarInventory.toString();
-
-    }
-
-    public void selectedCar() {
         boolean carFound = true;
         Scanner CarInput = new Scanner(System.in);
-        while (CarInput.hasNext() & carFound){
-            System.out.println("Here's the list of this FNCD's inventory. Enter the name of the vehicle to see its details. Or enter 0 to quit.");
+        System.out.println("Here's the list of this FNCD's inventory. Enter the name of the vehicle to see its details. Or enter 0 to quit.");
+        while (carFound){
             String Car = CarInput.nextLine();
             if(Car.equals("0")){
-                break;
+                carFound = false;
+                CarInput.close();
+                String Bye = "Thank you for looking at the details";
+                return Bye;
             }
-            else if(Car != "0" & vehicleInventory.contains(Car)){
+            else if(StringInventory.contains(Car)){
                 for(int i = 0; i < vehicleInventory.size(); i++){
                     if(Car.equalsIgnoreCase(vehicleInventory.get(i).name)){
-                        out("Here are the details for the selected car:");
-                        out("Cleanliness: " + vehicleInventory.get(i).cleanliness.toString());
-                        out("Condition: " + vehicleInventory.get(i).condition.toString());
-                        out("Price: " + vehicleInventory.get(i).price.toString());
-                        out("Type: " + vehicleInventory.get(i).type.toString());
+                        CarDetails.append(vehicleInventory.get(i).cleanliness.toString() + "\n");
+                        CarDetails.append(vehicleInventory.get(i).condition.toString()+ "\n");
+                        CarDetails.append(vehicleInventory.get(i).price.toString()+ "\n");
+                        CarDetails.append(vehicleInventory.get(i).type.toString()+ "\n");
                         carFound = false;
-                        break;
+                        CarInput.close();
+                        return CarDetails.toString();
+                    }
+                }
+            }
+            else {
+                System.out.println("The car you are looking for is not here please make sure the name is typed correctly.");
+            }
+        }
+        CarInput.close();
+        return CarDetails.toString();
+    }
+}
+
+class BuyCar implements BuyerInterface,SysOut { //Uses the same formatted code as aboce to Buy a car 
+    public ArrayList<Vehicle> vehicleInventory = new ArrayList<>();
+    public ArrayList<String> StringInventory = new ArrayList<>();
+    StringBuilder CarInventory = new StringBuilder();
+    StringBuilder CarDetails = new StringBuilder();
+    public String execute(FNCD Choice) {
+        for (Vehicle i: Choice.inventory){
+            CarInventory.append(i.name + "\n");
+            vehicleInventory.add(i);
+            StringInventory.add(i.name);
+        }
+        boolean carFound = true;
+        Scanner CarInput = new Scanner(System.in);
+        while (CarInput.hasNextLine()){
+            System.out.println("Here's the list of this FNCD's inventory. Enter the name of the vehicle you wish to buy. Or enter 0 to quit.");
+            String Car = CarInput.nextLine();
+            if(Car.equals("0")){
+                carFound = false;
+                CarInput.close();
+                String Bye = "Thank you for looking at vehicles if you change your mind we're always here!";
+                return Bye;
+            }
+            else if(StringInventory.contains(Car)){
+                for(int i = 0; i < vehicleInventory.size(); i++){
+                    if(Car.equalsIgnoreCase(vehicleInventory.get(i).name)){
+                        String SelectedCar = vehicleInventory.get(i).name;
+                        carFound = false;
+                        CarInput.close();
+                        return SelectedCar.toString();
                     }
                 }
                 break;
@@ -72,40 +122,41 @@ class CurrentInventory implements BuyerInterface, SysOut{
             }
         }
         CarInput.close();
+        return CarDetails.toString();
     }
 }
-class CarDetails implements SysOut{
+
+ 
+class CarDetails implements SysOut{ //Displays car details 
     StringBuilder Details = new StringBuilder();
-    public void execute(FNCD Choice, Vehicle CarChoice){
+    public void execute(FNCD Choice, Vehicle CarChoice){ //Appends all the details of the car to the string 
         Details.append(CarChoice.type).append("\n");
         Details.append(CarChoice.cleanliness).append("\n");
         Details.append(CarChoice.condition).append("\n");
         Details.append(CarChoice.cost).append("\n");
-        out(Details.toString());
+        out(Details.toString()); //returns string 
     }
 }
 
 
-class BuyCar{
-    
-}
 
-class UserMenu implements SysOut {
+class UserMenu implements SysOut { //User menu command pattern
     public void Menu(FNCD Choice){
-        boolean Loop = true;
         Scanner UserInput = new Scanner(System.in);
         SalespersonName Name = new SalespersonName();
+        boolean Loop = true;
         while(Loop){
+
             out("Please enter one of the follow options.");
             out("Enter '1' if you wish to ask the salesperson their name.");  
             out("Enter '2' if you wish to ask the salesperson the time.");  
             out("Enter '3' if you wish to have a new salesperson help you."); 
             out("Enter '4' if you wish to ask for the inventory."); 
-            out("Enter '5' if you wish to see the details for a user Selected Item.");
+            out("Enter '5' if you wish to buy a car");
             out("Enter '6' if you wish to buy a car."); 
             out("Enter '7' if you wish to leave the FNCD.");
-
             int Decision = Integer.parseInt(UserInput.nextLine());
+
 
             if(Decision == 1){ //First Option a customer can choose
                 SalespersonName NewName = new SalespersonName();
@@ -121,21 +172,21 @@ class UserMenu implements SysOut {
                     while (NewName != Name){
                         NewName = new SalespersonName();
                     }
-                }
+                }  
             }
             else if (Decision == 4){
                 CurrentInventory Display = new CurrentInventory();
                 out("The current inventory of this FNCD location is ");
                 out(Display.execute(Choice) + "\n");
-                Display.selectedCar();
+                DisplaySelectedCar NDisplay = new DisplaySelectedCar();
+                out(NDisplay.execute(Choice));
             }
-
             
-            /*else if(Decision == 5){
-                Vehicle UserCar;
-                CarDetails ShowCar = new CarDetails();
-                out("The Details on the chosen car are");
-                out(ShowCar.execute(Choice,UserCar));
+            else if(Decision == 5){
+                CurrentInventory Display = new CurrentInventory();
+                out(Display.execute(Choice) + "\n");
+                BuyCar CarToBuy = new BuyCar();
+                out("You wish to buy the car " +CarToBuy.execute(Choice) + "?");
             }
             
             /*else if (Decision == 6){
@@ -150,6 +201,10 @@ class UserMenu implements SysOut {
     
             else{
                 out("Oops input is invalid try typing something else!");
+            }
+
+            if(!UserInput.hasNextLine()){
+                break;
             }
         }
 
